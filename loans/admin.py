b365957e10d60fe2171f9eda8e0fe0979cfbf4ca
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2023-10-21 12:34:36 +0800

    Add Loan model

--- loans/admin.py
+++ loans/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from loans.models import Loan
+
+
 # Register your models here.
+@admin.register(Loan)
+class LoanAdmin(admin.ModelAdmin):
+    readonly_fields = ["monthly_interest_rate", "monthly_payment", "smm"]
--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,66 @@
+from decimal import Decimal
+
+import numpy_financial as npf
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+from commons.utils import convert_to_decimal
+
+
+class PaymentFrequency(models.TextChoices):
+    MONTHLY = "Monthly"
+
+
+class Loan(models.Model):
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    annual_interest_rate = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+    )
+    start_date = models.DateField()
+    term = models.IntegerField()  # Term in months
+    payment_frequency = models.CharField(
+        max_length=10,
+        choices=PaymentFrequency.choices,
+        default=PaymentFrequency.MONTHLY,
+    )
+    cpr = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+    )
+    monthly_interest_rate = models.DecimalField(
+        max_digits=5, decimal_places=2, editable=False
+    )
+    monthly_payment = models.DecimalField(
+        max_digits=12, decimal_places=2, editable=False
+    )
+    smm = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        editable=False,
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+    )
+
+    def _calculate_monthly_interest_rate(self):
+        monthly_interest_rate = self.annual_interest_rate / 12
+        return monthly_interest_rate
+
+    def _calculate_monthly_payment(self):
+        negative = -self.amount
+        return npf.pmt(
+            convert_to_decimal(self.monthly_interest_rate), self.term, negative
+        )
+
+    def _calculate_smm(self):
+        # Calculate Single Monthly Mortality
+        cpr_decimal = convert_to_decimal(self.cpr)
+        smm = (1 - ((1 - cpr_decimal) ** Decimal(1 / 12))) * 100
+        return smm
+
+    def save(self, *args, **kwargs):
+        self.monthly_interest_rate = self._calculate_monthly_interest_rate()
+        self.monthly_payment = self._calculate_monthly_payment()
+        self.smm = self._calculate_smm()
+        super(Loan, self).save(*args, **kwargs)
