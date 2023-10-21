5c441e7d175a68990280f93ebf912a4ee4e3340d
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2023-10-21 23:07:54 +0800

    Add celery tasks

--- loans/tasks.py
+++ loans/tasks.py
@@ -1,14 +1,53 @@
+import logging
 from decimal import Decimal
 from typing import List
 
+import pandas as pd
+from celery import shared_task
 from dateutil.relativedelta import relativedelta
+from django.conf import settings
 from django.db import transaction
 
-from commons.utils import convert_to_decimal
-from loans.models import AmortizationSchedule, Loan
+from commons.utils import convert_to_decimal, convert_to_percent
+
+from .models import AmortizationSchedule, Loan, LoanExcel
+
+logger = logging.getLogger(__name__)
 
 
-def create_loan_amortization_schedule(loan: Loan):
+@shared_task
+def create_loans_and_amortizations(upload_id: int):
+    file: LoanExcel = LoanExcel.objects.get(id=upload_id)
+    df = pd.read_excel(file.excel_file.file, sheet_name=0)
+    df.columns = df.columns.str.strip()
+    df = df.loc[1:]
+    for index, row in df.iterrows():
+        loan_number = row[settings.LOAN_NUMBER]
+        logger.info("Creating Loan Number: %s", loan_number)
+        loan = Loan.objects.create(
+            loan_number=int(loan_number),
+            amount=Decimal(row[settings.LOAN_AMOUNT]),
+            annual_interest_rate=convert_to_percent(
+                Decimal(row[settings.INTEREST_RATE])
+            ),
+            start_date=row[settings.START_DATE],
+            term=int(row[settings.PAYMENT_TERM]),
+            payment_frequency=row[settings.PAY_FREQUENCY],
+            cpr=convert_to_percent(Decimal(row[settings.CPR])),
+        )
+        logger.info("Created Loan Number: %s", loan_number)
+
+        create_loan_amortization_schedule.delay(loan.id)
+
+    logger.info("Processing `%s` completed.", file.excel_file.name)
+
+
+@shared_task
+def create_loan_amortization_schedule(loan_id: int):
+    loan = Loan.objects.get(id=loan_id)
+    logger.info(
+        "Creating Amortization Schedules for Loan Number: %s", loan.loan_number
+    )
     schedules: List[AmortizationSchedule] = []
     for period in range(loan.term + 1):
         if not schedules:
@@ -66,3 +105,7 @@ def create_loan_amortization_schedule(loan: Loan):
 
     with transaction.atomic():
         AmortizationSchedule.objects.bulk_create(schedules)
+
+    logger.info(
+        "Created Amortization Schedules for Loan Number: %s", loan.loan_number
+    )
