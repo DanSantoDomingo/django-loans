e50af718bf6d1b32a12ac81fe672b362a36ca3d3
Dan Sto. Domingo <danielesto.domingo@gmail.com>
2023-10-21 19:35:26 +0800

    Add django admin for Amortization Schedule

--- loans/admin.py
+++ loans/admin.py
@@ -1,9 +1,28 @@
 from django.contrib import admin
 
-from loans.models import Loan
+from loans.models import AmortizationSchedule, Loan
 
 
 # Register your models here.
 @admin.register(Loan)
 class LoanAdmin(admin.ModelAdmin):
     readonly_fields = ["monthly_interest_rate", "monthly_payment", "smm"]
+
+
+@admin.register(AmortizationSchedule)
+class AmortizationScheduleAdmin(admin.ModelAdmin):
+    list_filter = ["loan"]
+    list_display = [
+        "id",
+        "loan",
+        "period",
+        "date",
+        "opening_balance",
+        "payment",
+        "pre_payment",
+        "interest_rate",
+        "monthly_interest_rate",
+        "interest",
+        "principal",
+        "closing_balance",
+    ]
